da48323273019d92620df4cb2ca9fbddb21a411e
Kyle Lamoureux <klamoureux386@gmail.com>
2023-06-19 20:00:27 -0400

    dialog manager rework + add dialog as struct

--- Source/TestRPG/DialogManager.cpp
+++ Source/TestRPG/DialogManager.cpp
@@ -3,34 +3,56 @@
 
 #include "DialogManager.h"
 
-DialogManager::DialogManager()
+// Sets default values for this component's properties
+UDialogManager::UDialogManager()
 {
-	TArray<Dialog> dialogs =
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	TArray<FDialog> dialogs =
 	{
-		Dialog(1, "a", 50, 2),
-		Dialog(2, "b", 50, 3)
+		FDialog(1, "a", 50, 2),
+		FDialog(2, "b", 50, 3)
 	};
 
-	for (Dialog d : dialogs) 
+	for (FDialog d : dialogs)
 	{
 		DialogLookup.Add(d.Id, d);
 	}
+}
+
+
+// Called when the game starts
+void UDialogManager::BeginPlay()
+{
+	Super::BeginPlay();
 
 	GetDialogById(3);
+	
+}
+
+
+// Called every frame
+void UDialogManager::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 }
 
-DialogManager::~DialogManager()
+
+FDialog::FDialog()
 {
 }
 
-Dialog DialogManager::GetDialogById(int32 id) 
+
+FDialog UDialogManager::GetDialogById(int32 id)
 {
-	Dialog* dialog = DialogLookup.Find(id);
+	FDialog* dialog = DialogLookup.Find(id);
 
 	if (dialog == nullptr)
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Unable to find Dialog with Id " + id));
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Unable to find Dialog with Id " + FString::FromInt(id)));
 
-	return dialog != nullptr ? *dialog : Dialog();
+	return dialog != nullptr ? *dialog : FDialog();
 
 }
--- Source/TestRPG/DialogManager.h
+++ Source/TestRPG/DialogManager.h
@@ -3,21 +3,58 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Dialog.h"
+#include "Components/ActorComponent.h"
+#include "DialogManager.generated.h"
 
-/**
- * 
- */
-class TESTRPG_API DialogManager : public UGameInstanceSubsystem
+USTRUCT()
+struct FDialog
 {
-public:
-	DialogManager();
-	~DialogManager();
+	GENERATED_USTRUCT_BODY()
 
-	Dialog GetDialogById(int id);
+public:
+	FDialog();
+
+	int Id;			//Unique Id for Dialog
+	FString Line;		//Content of subtitle
+	//Sound
+	int Duration;		//Duration of line to be shown as subtitle
+	int NextDialogId;	//Dialog to follow after completion of this Dialog
+
+	FDialog(int id, FString line, int duration, int nextDialog)
+	{
+		Id = id;
+		Line = line;
+		Duration = duration;
+		NextDialogId = nextDialog;
+	}
+
+	FDialog(int id, FString line, int duration)
+	{
+		Id = id;
+		Line = line;
+		Duration = duration;
+		NextDialogId = NULL;
+	}
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TESTRPG_API UDialogManager : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDialogManager();
 
 protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere, Category = "Dialog")
+	TMap<int32, FDialog> DialogLookup;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialog")
-	TMap<int32, Dialog> DialogLookup;
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	FDialog GetDialogById(int32 id);
+		
 };
